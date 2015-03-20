3999fad64bd4cf7fcecab2672ade60ecf40b07a5
MaxPovver <admin@MacBook-Pro-Maksim.local>
2015-03-20 16:58:41 +0300

    Потихоньку переношу логику из Swift

--- app/src/main/java/com/example/admin/testing2/DocumentsList.java
+++ app/src/main/java/com/example/admin/testing2/DocumentsList.java
@@ -1,4 +1,7 @@
 package com.example.admin.testing2;
+import android.util.JsonReader;
+
+import java.io.IOException;
 import java.util.ArrayList;
 /**
  * Created by admin on 20.03.15.
@@ -10,4 +13,20 @@ public class DocumentsList {
         return tree;
     }
     public ArrayList<SmallDocument> asCollection() { return docs;}
+    private void initWJ(JsonReader r) throws IOException {
+        int i=0;
+        while(!r.nextName().equals("total") || i++<500){r.skipValue();}//i чтобы не улететь в бесконечный цикл
+        i=0;
+        int count = r.nextInt();
+        docs = new ArrayList<SmallDocument>(count);
+        while(!r.nextName().equals("results") || i++<500){r.skipValue();}//i чтобы не улететь в бесконечный цикл
+        int j=0;
+        r.beginArray();
+        while(r.hasNext()) {
+            docs.add(new SmallDocument(r));
+        }
+        r.endArray();
+        tree = new DocTree(SmallDocument.root());
+        tree.addrange(docs);
+    }
 }
--- app/src/main/java/com/example/admin/testing2/SmallDocument.java
+++ app/src/main/java/com/example/admin/testing2/SmallDocument.java
@@ -1,5 +1,9 @@
 package com.example.admin.testing2;
 
+import android.util.JsonReader;
+
+import java.io.IOException;
+
 /**
  * Created by admin on 20.03.15.
  */
@@ -16,10 +20,50 @@ public class SmallDocument {
     private String _price = "";
     private String _format = "";
     /**********************************/
+    public SmallDocument(JsonReader r) throws IOException {
+        initWJ(r);
+    }
+    private void initWJ(JsonReader reader) throws IOException {
+       // JsonReader reader = new JsonReader(new StringReader(jsonstr));
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("id")) {
+                _id = reader.nextInt();
+            } else if (name.equals("pagetitle")) {
+                _title = reader.nextString();
+            } else if (name.equals("parent")) {
+                _parent = reader.nextInt();
+            } else if (name.equals("empty")) {
+                _empty = reader.nextBoolean();
+            } else if (name.equals("deleted")) {
+                _deleted = reader.nextBoolean();
+            } else if (name.equals("publishedon")) {
+                _publishedon = reader.nextLong();
+            } else if (name.equals("image")) {
+                _image = reader.nextString();
+            } else if (name.equals("coach")) {
+                _coach = reader.nextString();
+            } else if (name.equals("price")) {
+                _price = reader.nextString();
+            } else if (name.equals("format")){
+                _format = reader.nextString();
+            } else reader.skipValue();
+        }
+        reader.endObject();
+    }
+    private SmallDocument() {
 
-
-
-
+    }
+    public static SmallDocument root() {
+        SmallDocument sd = new SmallDocument();
+        sd._id = 0;
+        sd._title = "Меню";
+        sd._empty = true;
+        sd._deleted = false;
+        sd._publishedon = 0;
+        return sd;
+    }
     public int getID() {
         return _id;
     }
